0460f4f14edaef80784a8497f8875c4db8c16a27
Mack <amack@proforma.com>
2018-04-08 17:02:32 -0400

    Addind properties to my classes

---
+++ atmMachine/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atmMachine
+{
+    class Account
+    {
+        string name; //-name(string)
+        string id;   //-id(String)
+        double balance;  //-balance(double)
+
+
+        static int count;  //-static int count;
+
+        public Account() {
+            // empty contructor
+        }
+        public Account( string name, double initBalance)  //+idcontructor/generator (name,initial balance)
+        {
+            this.name = name;
+            this.balance = initBalance;
+            this.id = this.generateID();
+        }
+        public string getName() { //+get name()
+            return this.name;
+        }
+        public string generateID() { //+str create id()
+
+            return "dummy";
+        }
+        public double getBalance() //+get balance
+        {
+            return this.balance;
+        }
+        public string getID() //+get id
+        {
+            return this.id;
+        }
+        public bool deposit( double amount) { //+total bool deposit(amount)
+
+            return true;
+        }
+        public bool withdraw(double amount)  //+total bool withdraw(amount)
+        {
+
+            return true;
+        }
+    }
+}
---
+++ atmMachine/Bank.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atmMachine
+{
+    class Bank
+    {
+
+        public string ID { get; set; }
+        public string id { get; private set; }
+        public string name { get; private set; }
+        public string balance { get; private set; }
+
+        Account[] accounts;
+        public void createAccount() {
+
+        }
+
+        public bool deposit(double amount) { // bool deposit(amount)
+
+            return true;
+        }
+        public bool withdraw(double amount) {  //+ bool withdraw(amount)
+
+            return true;
+        }
+
+        public void showAccountInfo() {
+            string message = "The account ID: " + this.id;
+            this.message(message);
+
+            message = "Name: " + this.name;
+            this.message(message);
+
+            message = "Balance: " + this.balance;
+            this.message(message);
+        }
+        void message(string msg) {
+            System.Console.WriteLine(msg);
+        }
+    }
+}
